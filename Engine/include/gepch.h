d78821d50b54985080e8b7a1040ae6b93d0d8411
Brandon Hillig <brandonhillig@gmail.com>
2026-02-01 13:05:11 -0500

    Implement a precompiled header + more cleanup of log

---
+++ Engine/include/gepch.h
@@ -0,0 +1,28 @@
+#pragma once
+
+// Standard Library
+#include <iostream>
+#include <print>
+#include <sstream>
+#include <ranges>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <string_view>
+
+// Containers
+#include <array>
+#include <vector>
+#include <tuple>
+#include <string>
+#include <unordered_map>
+
+// GLM
+#include <glm/glm.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// STB Image
+#include <stb_image.h>
--- Engine/src/Log.cpp
+++ Engine/src/Log.cpp
@@ -17,7 +17,7 @@ bool Log::Initialize()
 
 	// Initialize Client Logger
 	s_clientLogger = spdlog::stdout_color_mt("APP");
-	s_coreLogger->set_level(spdlog::level::trace);
+	s_clientLogger->set_level(spdlog::level::trace);
 
 	return true;
 }
